afd50d6f148ae277a7a5de03e95abe01268b7253
Ngobidi <chibuzormaxwell@gmail.com>
2024-04-25 09:13:05 -0700

    updated

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,17 +1,20 @@
 export default class Building {
   constructor(sqft) {
-    if (
-      new.target !== Building
-      && typeof this.evacuationWarningMessage !== 'function'
-    ) {
-      throw new Error(
-        'Classes beyound Building must override evacuation-Warning-Message',
-      );
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
     }
-    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
   }
 }
