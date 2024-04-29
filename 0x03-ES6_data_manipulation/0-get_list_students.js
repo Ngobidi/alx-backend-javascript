f608150a14245f497d36a705c2e4ea88004f2f3c
Ngobidi <chibuzormaxwell@gmail.com>
2024-04-29 12:56:07 -0700

    0x03. ES6 data manipulation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,12 @@
+/**
+ * Recover a list of students.
+ * @author Ngobidi Chibuzor <https://github.com/Ngobidi>
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ */
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
