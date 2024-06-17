e0d58c52e64dc602bdab83e072892b28c0999089
Ngobidi <chibuzormaxwell@gmail.com>
2024-06-17 00:46:09 -0700

    Node_JS_basic_TASK

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * show a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Ngobidi chibuzor <https://github.com/Ngobidi>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,12 @@
+/**
+ * Contains the miscellaneous route handlers.
+ * @author Ngobidi chibuzor <https://github.com/ngobidi>
+ */
+class AppController {
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
+  }
+}
+
+export default AppController;
+module.exports = AppController;
